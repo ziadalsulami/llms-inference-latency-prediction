22d4245e03d9af591ffb67e2951840019544d67d
ziadalsulami <ziadu.alsulami@gmail.com>
2026-06-27 20:19:58 +0300

    load and split data

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hi")
+from datasets import load_dataset
+import pandas as pd
+ds = load_dataset('rbgo/llm-inference-benchmark')
+
+
+split = ds['train'].train_test_split(train_size=0.2, seed=42)
+
+df_train = split['train'].to_pandas()
+df_test = split['test'].to_pandas()
+
+print(df_train.head())
